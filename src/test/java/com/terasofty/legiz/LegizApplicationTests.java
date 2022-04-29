dabb2f3c2a1785fd7e61e26c22674287249b0046
Alessandro Paris <par_214@hotmail.com>
2022-04-29 13:38:33 -0500

    Starting Testing Suite

--- src/test/java/com/terasofty/legiz/LegizApplicationTests.java
+++ src/test/java/com/terasofty/legiz/LegizApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LegizApiApplicationTests {
+class LegizApplicationTests {
 
     @Test
     void contextLoads() {
