53b60f9edf49a1d273251e654e4a13962a109780
Alessandro Paris <par_214@hotmail.com>
2022-04-29 11:50:36 -0500

    Added Swagger config to support JWT

--- src/main/java/com/terasofty/legiz/controllers/HiringsController.java
+++ src/main/java/com/terasofty/legiz/controllers/HiringsController.java
@@ -1,12 +1,14 @@
 package com.terasofty.legiz.controllers;
 
+import io.swagger.annotations.Api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController(value = "hirings")
+@Api( tags = "Hirings")
 public class HiringsController {
-    @GetMapping(value = "hiring")
+    @GetMapping
     public ResponseEntity<String> getAll() {
         return ResponseEntity.ok("AAAA");
     }
