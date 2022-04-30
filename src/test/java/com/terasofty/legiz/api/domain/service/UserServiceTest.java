82f8541ea40e4d727ad460d9e70503ae11c084dd
Alessandro Paris <par_214@hotmail.com>
2022-04-29 19:36:29 -0500

    Added some Tests

--- src/test/java/com/terasofty/legiz/api/domain/service/UserServiceTest.java
+++ src/test/java/com/terasofty/legiz/api/domain/service/UserServiceTest.java
@@ -7,36 +7,50 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-@RunWith(SpringRunner.class)
 class UserServiceTest {
     @Mock
     private UserRepository userRepository;
     @Mock
-    private RoleRepository roleRepository;
-    private User user;
+    private PasswordEncoder passwordEncoder;
+    private User user, user2;
     @InjectMocks
     private UserServiceImpl userService;
     @BeforeEach
     void setUp(){
         MockitoAnnotations.openMocks(this);
-        user = new User();
-        user.setId(1L);
-        user.setName("Alessandro");
-        user.setUsername("hyper");
-
+        user = new User(1L, "Alessandro", "hyper", "12345", new ArrayList<>());
+        user2 = new User(2L, "John", "john", "12345", new ArrayList<>());
     }
     @Test
     void getUsers() {
-        System.out.println(userService.getUsers());
-        when(userRepository.findAll()).thenReturn(List.of(user));
-        assertNotNull(userService.getUsers());
+        when(userRepository.findAll()).thenReturn(List.of(user, user2));
+        List<User> users = userService.getUsers();
+        assertNotNull(users);
+        assertEquals(users.size(), 2);
+    }
+
+    @Test
+    void saveUser() {
+        when(userRepository.save(Mockito.any(User.class)))
+                .thenReturn(user);
+        System.out.println(userRepository.findByUsername(user.getUsername()));
+        //assertEquals(userRepository.findOne(createdUser.getUsername()), 1);
+
+    }
+
+    @Test
+    void getUser() {
     }
 }
