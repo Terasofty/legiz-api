13f54f287cff28ac4d3ce6d9f3f153c06903f1ae
Alessandro Paris <par_214@hotmail.com>
2022-04-30 16:41:23 -0500

    Added Lawyers Resources

---
+++ src/main/java/com/terasofty/legiz/api/domain/service/LawyersServiceImpl.java
@@ -0,0 +1,33 @@
+package com.terasofty.legiz.api.domain.service;
+
+import com.terasofty.legiz.api.domain.models.Lawyer;
+import com.terasofty.legiz.api.domain.models.Specialization;
+
+import java.util.List;
+
+public class LawyersServiceImpl implements LawyersService{
+    @Override
+    public List<Lawyer> getLawyers() {
+        return null;
+    }
+
+    @Override
+    public Lawyer getLawyer(String username) {
+        return null;
+    }
+
+    @Override
+    public Lawyer createLawyer(Lawyer lawyer) {
+        return null;
+    }
+
+    @Override
+    public Lawyer deleteLawyer(Lawyer lawyer) {
+        return null;
+    }
+
+    @Override
+    public void addSpecializationToLawyer(Lawyer lawyer, Specialization specialization) {
+
+    }
+}
